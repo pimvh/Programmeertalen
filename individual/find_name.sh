b44f40fe27f5768a2ed073a545417139e7bd0c61
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2020-02-05 09:29:56 +0100

    working find_name.sh

--- individual/find_name.sh
+++ individual/find_name.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+# find_name.sh: Locates matching string in all java files.
+echo
+
+# go to upper dir a and search upper directory for java files
+cd ..
+
+FILES="**/*.java *.java"
+# count, print file name, print line number, fixed string
+GREP_OPTS="-Hon"
+
+E_BADARGS=65
+
+if [ $# -ne 1 ]
+then
+  echo "Usage: `basename $0` search_string"
+  exit $E_BADARGS
+fi
+
+for file in $FILES
+do
+    grep $GREP_OPTS "$1" "$file"
+done
+
+exit $?
