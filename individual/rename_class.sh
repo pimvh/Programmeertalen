1787e32fc3b3a1621d9780b9d1f26e8d5fa75a72
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2020-02-05 12:43:21 +0100

    wrote rename_class script

--- individual/rename_class.sh
+++ individual/rename_class.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+E_BADARGS=65
+
+if [ $# -ne 2 ]
+then
+  echo "Usage: `basename $0` [old-name] [new-name]"
+  exit $E_BADARGS
+fi
+
+cd ..
+
+FILE=$(find -name $1'.java')
+
+NEW_FILENAME=$(echo $FILE | perl -pe "s/$1/$2/s")
+cat $FILE | perl -pe "s/class\s$1/class $2/sg" > $NEW_FILENAME
+rm $File
+
+exit $?
