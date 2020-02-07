8c0c8c40fac7747e13d5410b2f15c4fe0dce035a
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2020-02-07 16:48:27 +0100

    changed hc_statistics.sh

--- team/hc_statistics.sh
+++ team/hc_statistics.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
+DIR_NAME="hc-logs"
+FILE_PREFIX="param"
+FILE_EXTENSION='*.dat'
+
+HEADER_END_STRING='Knapsack'
+
+GREP_OPTS="-hon -m 1"
+if [ $# -ne 0 ]
+then
+  echo "Usage: `basename $0`"
+  exit $E_BADARGS
+fi
+
+# AWK_ARG="-verbose $VERBOSE -nr_items $NR_ITEM -dimensions $DIMENSIONS -solver $SOLVER -nr_iterations $NR_ITERATIONS"
+
+cd ..
+
 #create directory
 if [ ! -e "$DIR_NAME" ]       # Check if dir exists.
 then
-    echo "creating $DIR_NAME..."
+    echo "$DIR_NAME does not exit, please run ./hc_experiments.sh first."
     exit 1
 fi
+
+cd hc-logs
+
+FILES=$(find -name "$FILE_EXTENSION")
+
+for file in $FILES
+do
+    echo $file
+    # find 'Knapsack' in file, and start reading from the line below it.
+    HEADER=$(($(grep $GREP_OPTS $HEADER_END_STRING $file | cut -f 1 -d:) + 2))
+    tail $file --lines=+$HEADER | cut -d' ' -f1,5 > temp.txt
+done
