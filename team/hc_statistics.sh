b3fdeb17aa2d504465a1a80e4a159131ee39de2c
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2020-02-07 17:50:12 +0100

    finished hc_statistics.sh, successfully creates a files now

--- team/hc_statistics.sh
+++ team/hc_statistics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR_NAME="hc-logs"
-FILE_PREFIX="param"
+FILE_PREFIX="final"
 FILE_EXTENSION='*.dat'
 
 HEADER_END_STRING='Knapsack'
@@ -14,23 +14,32 @@ then
 fi
 
 # AWK_ARG="-verbose $VERBOSE -nr_items $NR_ITEM -dimensions $DIMENSIONS -solver $SOLVER -nr_iterations $NR_ITERATIONS"
-
 cd ..
 
-#create directory
 if [ ! -e "$DIR_NAME" ]       # Check if dir exists.
 then
     echo "$DIR_NAME does not exit, please run ./hc_experiments.sh first."
     exit 1
 fi
 
-FILES=$(find -name "$FILE_EXTENSION")
+FILES=$(find $DIR_NAME -name "$FILE_EXTENSION")
 
 for file in $FILES
 do
-    echo $file
     # find 'Knapsack' in file, and start reading from the line below it.
     HEADER=$(($(grep $GREP_OPTS $HEADER_END_STRING $file | cut -f 1 -d:) + 2))
-    tail $file --lines=+$HEADER | cut -d' ' -f1,5 | awk -F '[ :]' '{print $2" "$4}'  > $DIR_NAME/temp.txt
-    # | cut -d':' -f2,3
+    tail $file --lines=+$HEADER | cut -d' ' -f1,5 | awk -F '[ :]' '{print $2" "$4}'  > $DIR_NAME/cleaned.txt
+
+    # build new file name
+    PARAM=$(echo $file | echo $(cut -d'-' -f3))
+    AVG_FILE_NAME="final-$PARAM.dat"
+
+    # find the final value in cleaned file
+    tail $DIR_NAME/cleaned.txt --lines=2 | awk -F" " '{print $2}' | head -1 >> $DIR_NAME/$AVG_FILE_NAME
+
+    # update the original file
+    cat $DIR_NAME/cleaned.txt > $file
 done
+
+# remove last temporary file
+rm $DIR_NAME/cleaned.txt
